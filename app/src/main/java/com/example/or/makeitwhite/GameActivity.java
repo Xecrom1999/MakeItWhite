0ceaa3677b84b89772309799a25f6a6ec16065c0
oriyair1 <oriyair1@gmail.com>
2016-10-30 18:21:10 +0200

    123

--- app/src/main/java/com/example/or/makeitwhite/GameActivity.java
+++ app/src/main/java/com/example/or/makeitwhite/GameActivity.java
@@ -1,9 +1,11 @@
 package com.example.or.makeitwhite;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Chronometer;
 
 import java.util.Random;
 
@@ -37,12 +39,17 @@ public class GameActivity extends AppCompatActivity implements Communicator {
             Random rnd = new Random();
             num = rnd.nextInt(4);
             if(fragments[num].active) f = true;
-            if(!fragments[0].active && !fragments[1].active && !fragments[2].active && !fragments[3].active) return;
+            if(!fragments[0].active && !fragments[1].active && !fragments[2].active && !fragments[3].active){
+                Intent i = new Intent(this, GameOverActivity.class);
+                //
+                startActivity(i);
+            }
         }
         fragments[num].setAsClickable();
     }
 
     public void onC(View v){
         requestRandomClick();
+
     }
 }
