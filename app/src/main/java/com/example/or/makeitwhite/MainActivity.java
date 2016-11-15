1a7d1ee16b1b4ae6657b79bfeca626c44e60b68f
gamrian <arielg1000>
2016-11-16 00:25:18 +0200

    123

--- app/src/main/java/com/example/or/makeitwhite/MainActivity.java
+++ app/src/main/java/com/example/or/makeitwhite/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.or.makeitwhite;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -14,6 +16,7 @@ import com.google.android.gms.ads.MobileAds;
 public class MainActivity extends AppCompatActivity {
 
     TextView logo;
+    TextView best_score_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
         logo = (TextView)findViewById(R.id.make_it_white);
         Typeface myFont = Typeface.createFromAsset(getAssets(), "fonts/logo_font.ttf");
         logo.setTypeface(myFont);
+
+        SharedPreferences preferences = getSharedPreferences("Data", MODE_PRIVATE);
+
+        int bestScore = preferences.getInt("bestScore", 0);
+
+        best_score_text = (TextView) findViewById(R.id.best_score_text);
+
+        best_score_text.setText("Best score: " + bestScore);
     }
 
     public void onClick(View v){
